5c9451f0e5ed579283cafeb87f3d0a49eb1476ac
vbarahona10 <vbarahona10_@hotmail.com>
2020-07-27 17:59:10 -0600

    Version 1.0 by vbarahona10

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:quizstar/splash.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "ECURA",
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: splashscreen(),
+    );
+  }
+}
+
+class Myapp extends StatelessWidget {
+  //final String title;
+
+  //MyApp({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Messages'),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main_drawer.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:quizstar/home.dart';
+import 'details_screen.dart';
+
+class mainDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            color: Theme.of(context).primaryColor,
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: EdgeInsets.only(
+                      top: 30,
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        Material(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50.0)),
+                            child: Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Image.asset(
+                                "images/ecura.jpeg",
+                                width: 80,
+                                height: 80,
+                              ),
+                            ))
+                      ],
+                    ),
+                  ),
+                  Text(
+                    'Ecura',
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Perfil',
+                style: TextStyle(
+                  fontSize: 18,
+                )),
+            onTap: null,
+          ),
+          ListTile(
+            leading: Icon(Icons.games),
+            title: Text('Modulos',
+                style: TextStyle(
+                  fontSize: 18,
+                )),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => homepage(),
+                  ));
+              //Navigator.pushNamed(context, '/details_screen');
+              //Navigator.of(context).pushNamed(DetailsScreen.routename);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.info),
+            title: Text('Acerca de',
+                style: TextStyle(
+                  fontSize: 18,
+                )),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailsScreen(),
+                  ));
+              //Navigator.pushNamed(context, '/details_screen');
+              //Navigator.of(context).pushNamed(DetailsScreen.routename);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
