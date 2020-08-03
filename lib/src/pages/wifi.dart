3e441c9bf6a6dd8c9d4aed58abbd118c7be06f96
rudyfvc <rudyfvasquezc@gmail.com>
2020-08-03 17:59:44 -0600

    video wifi

--- lib/src/pages/wifi.dart
+++ lib/src/pages/wifi.dart
@@ -38,7 +38,7 @@ class VideoState extends State<Wifi> {
       playerController = VideoPlayerController.asset(
           // "https://r3---sn-vgqsknez.googlevideo.com/videoplayback?source=youtube&mime=video%2Fmp4&itag=22&key=cms1&requiressl=yes&beids=%5B9466592%5D&ratebypass=yes&fexp=9466586,23724337&ei=g3jiWvfCL4O_8wScopaICA&signature=43C209DD37289D74DB39A9BBF7BC442EAC049426.14B818F50F4FA686C13AF5DD1C2A498A9D64ECC9&fvip=3&pl=16&sparams=dur,ei,expire,id,initcwndbps,ip,ipbits,ipbypass,itag,lmt,mime,mip,mm,mn,ms,mv,pl,ratebypass,requiressl,source&ip=54.163.50.118&lmt=1524616041346022&expire=1524813027&ipbits=0&dur=1324.768&id=o-AJvotKVxbyFDCz5LQ1HWQ8TvNoHXWb2-86a_50k3EV0f&rm=sn-p5qyz7s&req_id=e462183e4575a3ee&ipbypass=yes&mip=96.244.254.218&redirect_counter=2&cm2rm=sn-p5qe7l7s&cms_redirect=yes&mm=34&mn=sn-vgqsknez&ms=ltu&mt=1524791367&mv=m")
           //"videos/Butterfly.mp4")
-          "videos/spam.mp4")
+          "videos/wifi.mp4")
         //"https://www.youtube.com/watch?v=HHEwbIp4ICo&feature=youtu.be")
         ..addListener(listener)
         ..setVolume(1.0)
